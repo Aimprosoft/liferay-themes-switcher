5e71cbc286e36d6d591603832952d28ae182e466
v.koshelenko <v.koshelenko@aimprosoft.com>
2017-10-18 17:09:03 +0300

    License update

--- src/main/java/com/aimprosoft/lfs/model/view/ThemeOption.java
+++ src/main/java/com/aimprosoft/lfs/model/view/ThemeOption.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html/>.
  */
 
 package com.aimprosoft.lfs.model.view;
